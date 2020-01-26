0abd7ce6e469341065334e2b213bad47d1d5615a
Kotturi <Jyothi.Kotturi@parexel.com>
2020-01-26 21:36:44 +0530

    Browser

--- src/test/java/com/qa/test/DemoTest.java
+++ src/test/java/com/qa/test/DemoTest.java
@@ -1,15 +1,21 @@
 package com.qa.test;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class DemoTest {
-
 	@Test
-public void addd() {
-		int a,b,c;
-		a=10;
-		b=20;
-				c=a+b;
-		System.out.println("Sum: "+c);
+	public void login() {
+		// Initialize browser
+	
+		System.setProperty("webdriver.chrome.driver", "C:\\Jyothi\\Softwares\\Browsers\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		// Open facebook
+		driver.get("http://www.facebook.com");
+		 
+		// Maximize browser
+		 
+		driver.manage().window().maximize();
 	}
-}
+	}
