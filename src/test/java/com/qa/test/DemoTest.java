56ecf2eb14a1bc3e0e52365caa32909824dc0dc2
Kotturi <Jyothi.Kotturi@parexel.com>
2020-01-26 15:55:17 +0530

    SeleniumMavenGit1

---
+++ src/test/java/com/qa/test/DemoTest.java
@@ -0,0 +1,15 @@
+package com.qa.test;
+
+import org.testng.annotations.Test;
+
+public class DemoTest {
+
+	@Test
+public void addd() {
+		int a,b,c;
+		a=10;
+		b=20;
+				c=a+b;
+		System.out.println("Sum: "+c);
+	}
+}
---
+++ src/test/java/com/qa/test/DemoTest2.java
@@ -0,0 +1,14 @@
+package com.qa.test;
+
+import org.testng.annotations.Test;
+
+public class DemoTest2 {
+	@Test
+public void sub() {
+		int a,b,c;
+		a=10;
+		b=20;
+				c=b-a;
+		System.out.println("Subtration: "+c);
+	}
+}
