2c3abc323c8ddb17f76f4849f3ea295f78fb157f
Kotturi <Jyothi.Kotturi@parexel.com>
2020-01-27 23:07:56 +0530

    Two Files integration

--- src/test/java/com/qa/test/DemoTest.java
+++ src/test/java/com/qa/test/DemoTest.java
@@ -7,23 +7,32 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class DemoTest {
-	@Test
-	public void login() {
+	WebDriver driver;
+
+	@Test(priority=1)
+	public void url() throws InterruptedException {
 		// Initialize browser
-	
 		System.setProperty("webdriver.chrome.driver", "C:\\Jyothi\\Softwares\\Browsers\\chromedriver.exe");
-		WebDriver driver=new ChromeDriver();
+		driver = new ChromeDriver();
+		//driver.get("http://not-devweb-903v:17055/flex/LoginPasscode.aspx");
+		driver.get("https://www.facebook.com/");
 		
-		driver.get("http://not-devweb-903v:17055/flex/LoginPasscode.aspx");
-		 
 		driver.manage().window().maximize();
-		
-		WebElement username= driver.findElement(By.name("UserName"));
-		username.click();
-		username.sendKeys("devapp");
-		WebElement password= driver.findElement(By.name("Password"));
+		System.out.println("Open URL");
+		Thread.sleep(3000);
+	}
+
+	@Test(priority=2)
+	public void login() {
+		WebElement username= driver.findElement(By.name("firstname"));
+	//	WebElement username = driver.findElement(By.name("UserName"));
+	//	username.click();
+		username.sendKeys("Jyothi");
+	/*	WebElement password = driver.findElement(By.name("Password"));
 		password.click();
 		password.sendKeys("");
-		driver.findElement(By.name("Submit")).click();
+		driver.findElement(By.name("Submit")).click();*/
+		System.out.println("Login Successfull");
+		driver.close();
 	}
-	}
+}
--- src/test/java/com/qa/test/DemoTest2.java
+++ src/test/java/com/qa/test/DemoTest2.java
@@ -3,12 +3,12 @@ package com.qa.test;
 import org.testng.annotations.Test;
 
 public class DemoTest2 {
-	@Test
+	@Test(priority=3)
 public void sub() {
 		int a,b,c;
 		a=10;
 		b=20;
-				c=b-a;
+		c=b-a;
 		System.out.println("Subtration: "+c);
 	}
 }
