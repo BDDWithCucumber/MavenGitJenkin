0d0d160e8e728ac509c898a8642ac484aa859694
Kotturi <Jyothi.Kotturi@parexel.com>
2020-01-27 11:51:42 +0530

    Login to CP4

--- src/test/java/com/qa/test/DemoTest.java
+++ src/test/java/com/qa/test/DemoTest.java
@@ -1,6 +1,8 @@
 package com.qa.test;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
@@ -11,11 +13,17 @@ public class DemoTest {
 	
 		System.setProperty("webdriver.chrome.driver", "C:\\Jyothi\\Softwares\\Browsers\\chromedriver.exe");
 		WebDriver driver=new ChromeDriver();
-		// Open facebook
-		driver.get("http://www.facebook.com");
-		 
-		// Maximize browser
+		
+		driver.get("http://not-devweb-903v:17055/flex/LoginPasscode.aspx");
 		 
 		driver.manage().window().maximize();
+		
+		WebElement username= driver.findElement(By.name("UserName"));
+		username.click();
+		username.sendKeys("devapp");
+		WebElement password= driver.findElement(By.name("Password"));
+		password.click();
+		password.sendKeys("");
+		driver.findElement(By.name("Submit")).click();
 	}
 	}
