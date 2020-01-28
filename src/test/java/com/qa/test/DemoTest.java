d78881d4bfbc94a417c47dda422913878992633c
Kotturi <Jyothi.Kotturi@parexel.com>
2020-01-28 16:42:17 +0530

    CP4 Login

--- src/test/java/com/qa/test/DemoTest.java
+++ src/test/java/com/qa/test/DemoTest.java
@@ -14,9 +14,7 @@ public class DemoTest {
 		// Initialize browser
 		System.setProperty("webdriver.chrome.driver", "C:\\Jyothi\\Softwares\\Browsers\\chromedriver.exe");
 		driver = new ChromeDriver();
-		//driver.get("http://not-devweb-903v:17055/flex/LoginPasscode.aspx");
-		driver.get("https://www.facebook.com/");
-		
+		driver.get("http://not-devweb-903v:17055/flex/LoginPasscode.aspx");
 		driver.manage().window().maximize();
 		System.out.println("Open URL");
 		Thread.sleep(3000);
@@ -24,14 +22,13 @@ public class DemoTest {
 
 	@Test(priority=2)
 	public void login() {
-		WebElement username= driver.findElement(By.name("firstname"));
-	//	WebElement username = driver.findElement(By.name("UserName"));
-	//	username.click();
-		username.sendKeys("Jyothi");
-	/*	WebElement password = driver.findElement(By.name("Password"));
+		WebElement username = driver.findElement(By.name("UserName"));
+		username.click();
+		username.sendKeys("devapp");
+		WebElement password = driver.findElement(By.name("Password"));
 		password.click();
 		password.sendKeys("");
-		driver.findElement(By.name("Submit")).click();*/
+		driver.findElement(By.name("Submit")).click();
 		System.out.println("Login Successfull");
 		driver.close();
 	}
